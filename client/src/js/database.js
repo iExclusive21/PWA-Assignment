fdd00ec2f54b9b6879d13f835ef414154a1270c6
John Bynum <iexclusiveproductions@gmail.com>
2022-11-14 23:28:52 -0500

    modified syntax for putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  // TODO: Add logic to a method that accepts some content and adds it to the database
+
+  export const putDb = async (content) => {
+    console.log ('PUT to the database');
+
+    const contactDb =await openDB('jate', 1);
+
+    const tx = contactDb.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.put({ id: 1, value: content });
+
+    const result = await request;
+    console.log('data saved to the database', result);
+
+  };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
