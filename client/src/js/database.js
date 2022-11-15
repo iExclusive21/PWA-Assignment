d9d159626b32572157bf89ab2ab1d6ab4da61d2a
John Bynum <iexclusiveproductions@gmail.com>
2022-11-14 23:41:27 -0500

    completed database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,14 +14,15 @@ const initdb = async () =>
 
   // TODO: Add logic to a method that accepts some content and adds it to the database
 
+  // Added logic to Put function 
   export const putDb = async (content) => {
-    console.log ('PUT to the database');
+    console.log ('PUT request to update the Jate database');
 
     const contactDb =await openDB('jate', 1);
 
-    const tx = contactDb.transaction('jate', 'readwrite');
+    const transaction = contactDb.transaction('jate', 'readwrite');
 
-    const store = tx.objectStore('jate');
+    const objStore = tx.objectStore('jate');
 
     const request = store.put({ id: 1, value: content });
 
@@ -31,6 +32,22 @@ const initdb = async () =>
   };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+ 
+// Added logic to Get function 
+export const getDb = async () => {
+  console.log('Getting Data from Jate database');
+  
+  const contactDb = await openDB('jate', 1); 
+
+  const transaction = jateDb.transaction('jate', 'readwrite');
+
+  const objStore = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', results); 
+  return result?.value;
+}
 
 initdb();
