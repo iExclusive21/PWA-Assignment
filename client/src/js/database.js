33f01533247677fe1dd54995a21c4ee2faff817e
John Bynum <iexclusiveproductions@gmail.com>
2022-11-14 23:44:17 -0500

    Added comments to database page

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,15 +17,15 @@ const initdb = async () =>
   // Added logic to Put function 
   export const putDb = async (content) => {
     console.log ('PUT request to update the Jate database');
-
+    // connection to Database
     const contactDb =await openDB('jate', 1);
-
+    // New transaction with database and priveleges specified
     const transaction = contactDb.transaction('jate', 'readwrite');
-
+    // Open the desired object store 
     const objStore = tx.objectStore('jate');
-
+    // Passed in the content 
     const request = store.put({ id: 1, value: content });
-
+    // Confirmation of request going through
     const result = await request;
     console.log('data saved to the database', result);
 
@@ -36,7 +36,7 @@ const initdb = async () =>
 // Added logic to Get function 
 export const getDb = async () => {
   console.log('Getting Data from Jate database');
-  
+ 
   const contactDb = await openDB('jate', 1); 
 
   const transaction = jateDb.transaction('jate', 'readwrite');
